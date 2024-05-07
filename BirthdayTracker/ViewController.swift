93de4d784ae58fd15ef18f9dc9a86a115425fc0e
maksimir91 <maksimirrus@gmail.com>
2024-05-07 17:11:51 +0300

    Initial Commit

---
+++ BirthdayTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BirthdayTracker
+//
+//  Created by Stanislav Shut on 07.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
